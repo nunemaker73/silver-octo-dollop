36ee5edd50cf70c8a4d13a3d582e001f1f4f473a
Craig Nunemaker <nunemaker73@users.noreply.github.com>
2017-07-15 13:25:20 -0500

    add test target to makefile

---
+++ tests/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main (int argc, char *argv[])
+{
+	
+		std::cout << "Hello World>???\n";
+		std::cout << "we are testingi a program\n";
+		std::cout << "we will now exit with an error\n";
+		return 1;
+}
